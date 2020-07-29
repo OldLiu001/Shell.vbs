d2ed4ca3478b40e8a48217fd0fb164e8b6b02035
OldLiu001 <632171029@qq.com>
2020-07-29 13:49:20 +0800

    delete all files for fresh merge

--- Core/JavaScript/TextOutPut.js
+++
@@ -1,5 +0,0 @@
-function TextOutPut(strText) {
-	var pNode = document.createElement("p");
-	pNode.innerText = strText;
-	document.getElementById("Console").appendChild(pNode);
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Visual_Basic_Script_Shell
-A Shell can run VBS interactively, with syntax highlight, package management and common function library.
